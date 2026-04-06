a886e3f9ca7311cae5092b5ae156d377923c8b2d
CHEW ZHEN YANG <chewzhenyang1112@gmail.com>
2026-04-07 02:02:52 +0800

    today final input for checking number

--- day1.cpp
+++ day1.cpp
@@ -11,7 +11,20 @@ int main() {
     cout << "Please type your age : ";
     cin >> age;
 
+    while (cin.fail()) {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Invalid input. Please type again : ";
+        cin >> age;
+    }
+
     cout << "Hello My name is " << name <<". I am "<< age <<" years old.";
+
+    if (age >= 18){
+        cout << "\nYou are a big guys.";
+    } else {
+        cout << "\nYou cannot get any club yet";
+    }
 
     return 0;
 }
